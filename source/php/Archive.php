971d0cf30dd83d75ba4ec92411db8b6136aaaaf6
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-06-23 15:01:06 +0200

    Added edit button to archive pages

---
+++ source/php/Archive.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WpPageForPostType;
+
+class Archive
+{
+    public function __construct()
+    {
+        add_action('admin_bar_menu', array($this, 'addCustomAdminBarItems'), 80);
+    }
+
+    public function addCustomAdminBarItems()
+    {
+        $postId = get_option('page_for_' . get_post_type());
+
+        if(!is_archive() || $postId  === '0') {
+            return;
+        }
+
+        global $wp_admin_bar;
+
+        $wp_admin_bar->add_node(array(
+            'id'    => 'edit',
+            'title' => '<span class="ab-item"></span>Edit Page',
+            'href'  => get_edit_post_link($postId),
+        ));
+    }
+}
