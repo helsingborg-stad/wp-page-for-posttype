dfcea1d8c0ede61903db26a7f370cd07d9ec82d0
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-04-22 15:36:45 +0200

    Revert "fix: polylang archive links for localized page-for-post-type URLs (#9)"
    This reverts commit 3b8c748d049064b3f45e273ad87c9a74991d5168.

--- bootstrap.php
+++ bootstrap.php
@@ -1,24 +1,3 @@
 <?php
 
-declare(strict_types=1);
-
-$autoload = __DIR__ . '/vendor/autoload.php';
-
-if (file_exists($autoload)) {
-    require_once $autoload;
-}
-
-spl_autoload_register(static function (string $class): void {
-    $prefix = 'WpPageForPostType\\';
-
-    if (!str_starts_with($class, $prefix)) {
-        return;
-    }
-
-    $relativeClass = substr($class, strlen($prefix));
-    $filePath      = __DIR__ . '/source/php/' . str_replace('\\', '/', $relativeClass) . '.php';
-
-    if (file_exists($filePath)) {
-        require_once $filePath;
-    }
-});
+require_once './vendor/autoload.php';
